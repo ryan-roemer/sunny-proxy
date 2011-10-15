8b1d7d71485ed7984b194edce5156d5b195658f5
Ryan Roemer <ryan@loose-bits.com>
2011-10-15 10:06:02 -0400

    Server: Remove request variable and chain listeners and end() call for get container.

--- server.js
+++ server.js
@@ -12,40 +12,39 @@
     conn = sunny.Configuration.fromEnv().connection,  // Configuration.
     ADDR = process.env.ADDRESS || "0.0.0.0",          // Server settings.
     PORT = process.env.PORT || 2000,
-    CONTAINER = process.env.SUNNY_PROXY_CONTAINER,    // Serve this bucket.
-    request;
+    CONTAINER = process.env.SUNNY_PROXY_CONTAINER;    // Serve this bucket.
 
   // Get our container and create server inside to get blobs.
-  request = conn.getContainer(CONTAINER, { validate: true });
-  request.on('error', function (err) {
-    console.log(err);
-    throw err;
-  });
-  request.on('end', function (results) {
-    // We have a valid container, so let's create the server now.
-    http.createServer(function (req, res) {
-      var path = req.url.replace(/\/$/, "/index.html").replace(/^\/*/, ""),
-        status = 200,
-        logResult = function () { console.log("[%s] %s", status, path); },
-        stream;
+  conn.getContainer(CONTAINER, { validate: true })
+    .on('error', function (err) {
+      console.log(err);
+      throw err;
+    })
+    .on('end', function (results) {
+      // We have a valid container, so let's create the server now.
+      http.createServer(function (req, res) {
+        var path = req.url.replace(/\/$/, "/index.html").replace(/^\/*/, ""),
+          status = 200,
+          logResult = function () { console.log("[%s] %s", status, path); },
+          stream;
 
-      // Header based on MIME type (re-write on error).
-      res.writeHead(status, { 'content-type': mime.lookup(path) });
+        // Header based on MIME type (re-write on error).
+        res.writeHead(status, { 'content-type': mime.lookup(path) });
 
-      // Get blob and pass through error or pipe stream to response.
-      stream = results.container.getBlob(path);
-      stream.on('error', function (err) {
-        status = err.statusCode || 500;
-        res.writeHead(status, { 'content-type': "text/html" });
-        res.end("<html><h1>" + status + ": " + err.message + "</h1></html>");
-        logResult();
-      });
-      stream.on('end', logResult);
-      stream.pipe(res);
-      stream.end();
-    }).listen(PORT, ADDR);
-
-    console.log("Server running at http://%s:%s/", ADDR, PORT);
-  });
-  request.end();
+        // Get blob and pass through error or pipe stream to response.
+        stream = results.container.getBlob(path)
+        stream.pipe(res);
+        stream
+          .on('error', function (err) {
+            status = err.statusCode || 500;
+            res.writeHead(status, { 'content-type': "text/html" });
+            res.end("<h1>" + status + ": " + err.message + "</h1>");
+            logResult();
+          })
+          .on('end', logResult)
+          .end();
+      }).listen(PORT, ADDR);
+      console.log("Server running at http://%s:%s/", ADDR, PORT);
+    })
+    .end();
 }());
